7f22b28dbf3e42c5a16d6cdddccc19dab3a0482b
mallyy2003 <106653103+mallyy2003@users.noreply.github.com>
2023-05-20 22:12:47 +0530

    Add files via upload

---
+++ Exp11/Server.java
@@ -0,0 +1,113 @@
+	
+package com.jfr;
+
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import java.sql.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.lang.System.*;
+
+
+/**
+ *
+ * @author frsed
+ */
+@WebService(serviceName = "jfw")
+public class jfw  extends HttpServlet {
+
+
+    /**
+     * This is a sample web service operation
+     *
+     */
+    @WebMethod(operationName = "fetch")
+     public String[][] fetch() {                 //@WebParam(name = "name") String txt
+         
+         String[][] str = new String[4][4];
+        HttpServletResponse response = null;
+       String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://localhost:3307/bikes";
+
+
+        // Database credentials
+        String USER = "root";
+        String PASS = "";
+
+
+        String title = "Database Result";
+
+
+        System.out.println(title);
+        try {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+
+            // Open a connection
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+
+            // Execute SQL query
+            Statement stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM shows;";
+            ResultSet rs = stmt.executeQuery(sql);
+
+
+            System.out.println(" <style> th, td { ");
+            System.out.println("padding-top: 10px;");
+            System.out.println(" padding-bottom: 20px;");
+            System.out.println("padding-left: 30px;");
+            System.out.println("padding-right: 40px;");
+            System.out.println("} </style><center><div><table border = 1 >");
+            System.out.println("<tr><td> model </td>'");
+            System.out.println("<td> engine </td>");
+            System.out.println("<td> colour </td>");
+            System.out.println("<td> price </td></tr>");
+ 
+           
+            int i = 0;
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                String model = rs.getString("model");
+                String engine = rs.getString("engine");
+                String colour = rs.getString("colour");
+                String price = rs.getString("price");
+
+
+                // Display values
+                System.out.println("<tr> <td>" + model + " </td>");
+                System.out.println("<td>" + engine + "</td>");
+                System.out.println("<td>" + colour + "</td>");
+                System.out.println("<td >" + price + "</td>");
+                System.out.println("<td > ");
+                System.out.println("</td></tr> ");
+                int j = 0;
+               
+                str[i][j] = model;
+                str[i][j+1] = engine;
+                str[i][j+2] = colour;
+                str[i][j+3] = price;
+                i++;
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.print("Do not connect to DB - Error:" + e);
+        }
+        return str;
+    }
+
+
+       
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
